c417a0bc7cdcadf8d046bd193e8c163a34f8b91b
Yeun SeungHoo <yysh123@gmail.com>
2020-06-14 14:19:00 +0900

    유니티 유용한 스킬 배우기
    유튜브 케이디 영상 따라 배워보기
    
    1. 오브젝트 풀링
    2. HP 바
    3. 폭발 파편
    4. 무한 반복 배경
    5. 텍스쳐 스크롤링
    6. 카메라 흔들림
    7. 포물선 발사
    8. 마우스 휠 이동 & 카메라 줌아웃
    9. 자동발사 터렛
    10. 유도 미사일
    11. n단 점프
    12. AI 시야
    13. AI 순찰 및 특정지역(거점) 방어

---
+++ AdvancedSkill/Assets/1.ObjectPooling/Scripts/CreateCube1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube1 : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+
+    IEnumerator CreateCoroutine()
+    {
+        while(true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPollingManager1.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
